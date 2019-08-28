9ba1fda22830362380d83c73b5204510ef622ff3
nikpalyi <diavetito@gmail.com>
2019-08-28 14:57:08 +0100

    SigninAndSignUp page and SignIn component creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SigninAndSignupPage from './pages/sign-in and sign-up/sign-in and sign-up.component';
 
 import Header from './components/header/header.component';
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SigninAndSignupPage} />
       </Switch>
     </div>
   );
