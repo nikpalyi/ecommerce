2708ba5b9f2f9b7b47ac90b8024982fa5f35fca4
nikpalyi <diavetito@gmail.com>
2019-08-28 12:21:32 +0100

    HEADER and css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+import Header from './components/header/header.component';
+
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
@@ -9,9 +11,9 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
