7f086c6e6c32d7a59558da3a2b56f654758fb75a
Nikolett Palyi <nikpalyi@gmail.com>
2020-02-12 15:30:15 +0000

    fix sign-in-sign-up name

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
 
-import './sign-in and sign-up.styles.scss';
+import './sign-in-and-sign-up.styles.scss';
 
 const SigninAndSignupPage = () => (
   <div className='sign-in-and-sign-up'>
