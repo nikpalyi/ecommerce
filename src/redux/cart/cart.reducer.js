5a2735c3a0e1beb85aa97143df537d274fe94b02
Nikolett Palyi <nikpalyi@gmail.com>
2020-03-03 10:22:01 +0000

    added addItem and removeItem feature to checkout item component at dicrease-increase arrows

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 import CartActionTypes from './cart.types';
 
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 import { clearItemFromCart } from './cart.actions';
 
 const INITIAL_STATE = {
@@ -26,6 +26,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: state.cartItems.filter(
           cartItem => cartItem.id !== action.payload.id
         )
+      };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
     default:
       return state;
