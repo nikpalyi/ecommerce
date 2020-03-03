2bdd74986193b9ae1c614f0ca35866afea7d7e19
Nikolett Palyi <nikpalyi@gmail.com>
2020-03-03 12:12:24 +0000

    Redux persist, local storage, store caching, so cart stay persisting after refreshing app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
 
+//caching storage, using local storage
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+//user is not in whiteList, because user is cared by Firebase to persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
