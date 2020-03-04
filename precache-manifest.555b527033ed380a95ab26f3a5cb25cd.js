c1e112ae13350627f8a992c9255958f361cec4f2
Nikolett Palyi <nikpalyi@gmail.com>
2020-03-04 21:28:39 +0000

    Updates

---
+++ precache-manifest.555b527033ed380a95ab26f3a5cb25cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a12cff86200ef15aebbf7781edc58c",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "56cc39ae3c8977706976",
+    "url": "/ecommerce/static/css/main.44bbf794.chunk.css"
+  },
+  {
+    "revision": "45af179df86328a80083",
+    "url": "/ecommerce/static/js/2.32636316.chunk.js"
+  },
+  {
+    "revision": "0be9517239a5ca5873c3fb04a723bbd2",
+    "url": "/ecommerce/static/js/2.32636316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56cc39ae3c8977706976",
+    "url": "/ecommerce/static/js/main.fe26b19a.chunk.js"
+  },
+  {
+    "revision": "5c1bc07ad51b9044684b",
+    "url": "/ecommerce/static/js/runtime-main.c39edb80.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
