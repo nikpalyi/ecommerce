00737a7ce7cfc880b063a394944aca2ab17df310
Nikolett Palyi <nikpalyi@gmail.com>
2020-03-26 16:25:00 +0000

    commit

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -32,7 +32,9 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to='/shop'>SHOP</OptionLink>
       <OptionLink to='/contact'>CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+        <OptionLink as='div' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
       ) : (
         <OptionLink to='/signin'>SIGNIN</OptionLink>
       )}
